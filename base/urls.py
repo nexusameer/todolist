903dbdf43a2159c589fc2c9faf4263d7a538ac88
nexusameer <ameerk10fw@gmail.com>
2024-03-28 05:54:23 -0700

    Your commit message here

---
+++ base/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import *
+from django.contrib.auth.views import LogoutView
+
+urlpatterns =[
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    
+
+    path('', TaskList.as_view(), name='tasks' ),
+    path('task/<str:pk>', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<str:pk>', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<str:pk>', TaskDelete.as_view(), name='task-delete'),
+]
